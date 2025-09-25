a6cabf1c5f03349059ed9354f47d557e6932e071
JacobPark536 <jacobp149@nycstudents.net>
2025-09-25 14:14:51 +0000

    Update

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   
+  System.out.println("Jacob Park");
+  System.out.println("Disney World");
+  System.out.println("Jacob Park \nDisney World");
+    
+  }
+
+  
+}
